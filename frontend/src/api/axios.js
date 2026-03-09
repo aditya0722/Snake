abe7328832a19b738a1b03e7b4f52ca3dbd66100
Aditya Sharma <sharmaaaditya167@gmail.com>
2026-03-09 11:37:22 +0530

    urlfailed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost/Snake_bite/", // backend URL
+  baseURL: "https://snakebite-production.up.railway.app", // backend URL
 });
 
 export default api;
